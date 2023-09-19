30db00c7d1f6f617b1665717273c879d758aad6b
richardli03 <rli@olin.edu>
2023-09-18 22:18:43 -0400

    implement proportional control git add warmup_project/warmup_project/wall_following.py git add warmup_project/warmup_project/wall_following.py

--- warmup_project/warmup_project/wall_following.py
+++ warmup_project/warmup_project/wall_following.py
@@ -8,7 +8,11 @@ from sensor_msgs.msg import LaserScan
 from math import pi
 from enum import Enum, auto
 
+CORRECTIVE_CONSTANT = 0.35
+
+
 class States(Enum):
+    AGGRESSIVE_LEFT = auto()
     LEFT = auto()
     RIGHT = auto()
     STRAIGHT = auto()
@@ -16,20 +20,19 @@ class States(Enum):
 
 class WallFollower(Node):
     def __init__(self):
-        super().__init__('wall_follow')
-        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.odom_msg = self.create_subscription(Odometry, 'odom', self.update_position, 10)
-        self.scan_msg = self.create_subscription(LaserScan, 'scan', self.scan, 10)
-        timer_period = 1
-        self.timer = self.create_timer(timer_period, self.run_loop)
-        self.state = None
-        
-        
-    def move(self, lin = 0.0, ang = 0.0):
-        """move
+        super().__init__("wall_follow")
+        self.publisher = self.create_publisher(Twist, "cmd_vel", 10)
+        self.scan_msg = self.create_subscription(LaserScan, "scan", self.run_loop, 10)
 
-        :param lin: _description_
-        :type lin: _type_
+    def move(self, lin, ang):
+        """Move the robot with some linear velocity and some angular velocity.
+
+        Automatically converts the input to a float(as the Twist() message
+        only accepts floats), then publishes a message (hopefully to /cmd_vel)
+        that will move the robot accordingly.
+
+        :param lin: the linear velocity to give the robot
+        :type lin: int
         :param ang: _description_
         :type ang: _type_
         """
@@ -37,96 +40,35 @@ class WallFollower(Node):
         msg.linear.x = float(lin)
         msg.angular.z = float(ang)
         self.publisher.publish(msg)
-        
-    def scan(self, msg):
-        straight_ahead_dist = msg.ranges[0]
-        theta1_dist = msg.ranges[45]
-        theta2_dist = msg.ranges[45 + 90]
-        # print(straight_aead_dist)
-        print(f"THETA1 was {theta1_dist}, theta2_dist was {theta2_dist}")
-        
-        if straight_ahead_dist < 0.75:
-            # CRASHING INTO A WALL! OH NO!
-            self.turn(15) # turn 10 degrees to the left, should fix the issue
-        
-        if abs(theta1_dist - theta2_dist) < 0.5:
-            self.move(0.5, 0.0)
-            sleep(0.1)
-            
-        if theta1_dist > theta2_dist:
-            # print("turning right")
-            self.state = States.RIGHT
 
-        elif theta2_dist > theta1_dist:
-            # print("turning left")
-            self.state = States.LEFT
-            pass
-        else:
-            # print("moving straight")
-            self.state = States.STRAIGHT
-            pass
-        
-        
-    def update_position(self, msg):
+    def run_loop(self, msg):
         """
-        Set some attributes according to wheel encoder data.
+        From a scan message, determine what state the system
+        ought to exist in.
 
         :param msg: _description_
         :type msg: _type_
         """
-        self.xpos = msg.pose.pose.position.x
-        self.ypos = msg.pose.pose.position.y
-        self.zpos = msg.pose.pose.position.z
-        
-        self.orientation = msg.pose.pose.orientation
-        # print(self.xpos, self.ypos, self.zpos, self.orientation)
-        
-    def stop(self):
-        """Writing a stop method to make code more readable
-        """
-        self.move(0.0,0.0)
-        
-    def turn(self, angle):
-        """turn 
-
-        :param angle: angle in degrees
-        :type angle: int
-        """
-        
-        if angle < 0: 
-            angular_vel = -0.3 # NEG ANGLE MEANS RIGHT TURN
+        straight_ahead_dist = msg.ranges[0]
+        theta1_dist = msg.ranges[45]
+        theta2_dist = msg.ranges[135]  # 90 degrees past the previous
+        # print(straight_aead_dist)
+        print(f"THETA1 was {theta1_dist}, theta2_dist was {theta2_dist}")
+        if straight_ahead_dist < 0.5:
+            turn_mag = -5  # if we're stuck against a wall, turn until we're not
         else:
-            angular_vel = 0.3
-        time_to_sleep = (pi / angular_vel / 2) * (4) * (angle/360) # 90 degrees * 4 * (angle/360)
-    
-        self.move(0.0, angular_vel)
-        # print(f"sleeping: {abs(time_to_sleep)}")
-        sleep(abs(time_to_sleep))
-        self.move(0.0,0.0)
-        
-        
-    def run_loop(self):
-        print(f"state: {self.state}")
-        # self.turn(-2)
+            turn_mag = theta1_dist - theta2_dist
 
-        if self.state is not None:
-            if self.state == States.RIGHT:
-                self.turn(-2)
-            elif self.state == States.LEFT:
-                self.turn(2)
-            elif self.state == States.STRAIGHT:
-                self.move(1.0, 0.0)
-                sleep(0.5)
+        print(turn_mag)
+        self.move(0.1, turn_mag)
 
-                
-       
-        
-        
+
 def main(args=None):
-    rclpy.init(args=args) # init the node
+    rclpy.init(args=args)  # init the node
     node = WallFollower()
-    rclpy.spin(node) # starts up the node
-    rclpy.shutdown() # if it finishes, it'll shutdown
-    
-if __name__ == '__main__':
+    rclpy.spin(node)  # starts up the node
+    rclpy.shutdown()  # if it finishes, it'll shutdown
+
+
+if __name__ == "__main__":
     main()
